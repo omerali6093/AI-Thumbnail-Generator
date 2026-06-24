681dc4780f96eaaeadbf8c6034fad02d1f50bea4
Omer Ali <omerali200765@gmail.com>
2026-06-25 04:36:47 +0500

    feat: completed GeneatePage.tsx(added fetchThumbnail)

--- client/reactjs/src/pages/GeneratePage.tsx
+++ client/reactjs/src/pages/GeneratePage.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
+import { colorSchemes, dummyThumbnails, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
 import SoftBackDrop from "../components/SoftBackDrop";
 import AspectRatioSelector from "../components/AspectRatioSelector";
 import StyleSelector from "../components/StyleSelector";
@@ -20,6 +20,31 @@ function GeneratePage() {
     const [colorSchemeId, setColorSchemeId] = useState<string>(colorSchemes[0].id)
     const [style, setStyle] = useState<ThumbnailStyle>("Bold & Graphic")
     const [styleDropDown, setstyleDropDown] = useState(false)
+
+    const handleGenerate = async () => {
+
+    }
+    
+    const fetchThumbnail = async () => {
+        if(id) {
+            const thumbnail : any = dummyThumbnails.find((thumbnail) => thumbnail._id === id);
+            setThumbnail(thumbnail)
+            setAdditionalDetails(thumbnail.user_prompt)
+            setTitle(thumbnail.title)
+            setColorSchemeId(thumbnail.color_scheme)
+            setAspectRatio(thumbnail.aspect_ratio)
+            setStyle(thumbnail.style)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        if(id) {
+            fetchThumbnail()
+        }
+    }, [id])
+
+
 
     console.log(title)
 
@@ -93,7 +118,7 @@ function GeneratePage() {
 
                                 {/* BUTTON */}
                                 {!id && (
-                                    <button className="text-[15px] w-full py-3.5 rounded-xl 
+                                    <button onClick={handleGenerate} className="text-[15px] w-full py-3.5 rounded-xl 
                                     font-medium bg-linear-to-b from-pink-500 to-pink-600
                                      hover:from-pink-700 disabled:cursor-not-allowed transition-colors">
                                         {loading ? 'Generating...' : 'Generate Thumbnail'}
